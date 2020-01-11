26b35e623d573e1e6689f171af1aabc0da757ef1
sulthanalihsan <sulthanalihsan5@gmail.com>
2020-01-12 05:39:08 +0700

    first commit

---
+++ android/app/src/main/kotlin/myapp/sulthan/com/webinar1_projek_starter/MainActivity.kt
@@ -0,0 +1,12 @@
+package myapp.sulthan.com.webinar1_projek_starter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
